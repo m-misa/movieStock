e3c198bcccf4a1fc90f44340b4e9bf4b78f468c5
Misako Mimura <>
2022-09-15 19:43:09 +0900

    初回コミット

---
+++ HomeWork_20220915/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork_20220915
+//
+//  Created by Misako Mimura on 2022/09/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HomeWork_20220915UITests/HomeWork_20220915UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HomeWork_20220915UITestsLaunchTests.swift
+//  HomeWork_20220915UITests
+//
+//  Created by Misako Mimura on 2022/09/15.
+//
+
+import XCTest
+
+final class HomeWork_20220915UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

bc2b92945ac7a5f29bbbb6001f3fe4e39f25b02e
Misako Mimura <>
2022-10-30 20:34:10 +0900

    お気に入りタブ、ノートタブの追加、ファイルの追加

---
+++ HomeWork_20220915/Notes/NotesViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NotesViewModel.swift
+//  HomeWork_20220915
+//
+//  Created by Misako Mimura on 2022/10/30.
+//
+
+import Foundation
---
+++ HomeWork_20220915/Trend/Impression/ImpressionViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ImpressionViewModel.swift
+//  HomeWork_20220915
+//
+//  Created by Misako Mimura on 2022/10/30.
+//
+
+import Foundation
+
+protocol ImpressionViewModel {
+    
+}
+
+class ImpressionViewModelImpl:DetailViewModel {
+    
+}
